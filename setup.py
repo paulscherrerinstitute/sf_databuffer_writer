d711d6e6e2811fc2e75b9595a6cfbeac616937e8
Andrej Babic <andrej.babic@psi.ch>
2018-06-08 14:56:24 +0200

    New library version
    - Add adjusted data retrieval delay.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="sf_databuffer_writer",
-      version="0.0.1",
+      version="0.0.2",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
