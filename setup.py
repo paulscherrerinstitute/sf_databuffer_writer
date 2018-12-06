7cf599e05e113f08affb5c720e0107d7e15860a5
Andrej Babic <andrej.babic@psi.ch>
2018-12-06 14:40:47 +0100

    New library version
    - Add request for EPICS writer.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="sf_databuffer_writer",
-      version="0.0.5",
+      version="0.1.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
