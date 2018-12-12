60c4fd07addf03d53e4cfff024dcc7a2aef3f6c8
Dmitry Ozerov <Dmitry.Ozerov@psi.ch>
2018-12-12 10:34:59 +0100

    Improve manual broker test

--- tests/manual_broker_test.py
+++ tests/manual_broker_test.py
@@ -4,7 +4,7 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-def create_broker_request(channels, output_file, start_pulse_id, stop_pulse_id, rest_api_host):
+def create_broker_request(channels, output_file, start_pulse_id, stop_pulse_id, rest_api_host, compact):
 
     _logger.info("Broker hostname: %s", rest_api_host)
 
@@ -12,7 +12,8 @@ def create_broker_request(channels, output_file, start_pulse_id, stop_pulse_id, 
                   "general/user": "root",
                   "general/process": "manual_broker_test.py",
                   "general/instrument": "bsread",
-                  "output_file": output_file}
+                  "output_file": output_file,
+                  "output_file_format": "compact" if compact else None}
 
     _logger.info("Sending request with parameters: %s", parameters)
     requests.post("%s/parameters" % rest_api_host, json=parameters)
@@ -32,9 +33,15 @@ def run():
     parser.add_argument("start_pulse_id", type=int, help="Start pulse_id to write.")
     parser.add_argument("stop_pulse_id", type=int, help="Stop pulse_id to write.")
     parser.add_argument("--host", help="Hostname of broker rest API\nFormat: hostname:port", default="localhost:1002")
+    parser.add_argument("--compact", action='store_true', help="broker rest API\nFormat: hostname:port", default="localhost:1002")
+    parser.add_argument("--log_level", default="INFO",
+                        choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'], help="Log level to use.")
+
     args = parser.parse_args()
 
-    with open(arguments.channels_file) as input_file:
+    logging.basicConfig(level=args.log_level, format='[%(levelname)s] %(message)s')
+
+    with open(args.channels_file) as input_file:
         file_lines = input_file.readlines()
         channels = [channel.strip() for channel in file_lines
             if not channel.strip().startswith("#") and channel.strip()]
@@ -44,7 +51,8 @@ def run():
         output_file=args.output_file,
         start_pulse_id=args.start_pulse_id,
         stop_pulse_id=args.stop_pulse_id,
-        rest_api_host="http://%s"
+        rest_api_host="http://%s",
+        compact=args.compact
     )
 
 
