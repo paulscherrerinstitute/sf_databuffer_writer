2c2d379ff946563e3779fb9fd0efabd8648c2b20
Andrej Babic <andrej.babic@psi.ch>
2019-05-22 15:08:45 +0200

    New library version:
    - Add specific backend source.
    - Log server response if less then 1kb.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="sf_databuffer_writer",
-      version="0.2.0",
+      version="0.2.1",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
