bddffb4b1427500199ee749674b04ad677b6eb2a
Andrej Babic <andrej.babic@psi.ch>
2019-06-04 16:38:58 +0200

    New library version
    - Convert from pulse_id to timestamp range
    - Add flag to disable error exception.
    - Add flag to disable pulse_id conversion.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="sf_databuffer_writer",
-      version="0.2.1",
+      version="0.3.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
