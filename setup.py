2b123c72db73df67347f062e4c3db08dd5da266d
Andrej Babic <andrej.babic@psi.ch>
2019-06-07 16:46:27 +0200

    New library version
    - Separate image and bsread channels.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="sf_databuffer_writer",
-      version="0.3.0",
+      version="0.3.1",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
